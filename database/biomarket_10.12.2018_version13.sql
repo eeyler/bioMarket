d35b9b466a9ad1ae4ffb508321dd35f1770805f1
eeyler <edit.egri@yahoo.co.uk>
2018-12-10 02:21:44 +0000

    BioMarket - Team Project TEST version 13 Customers Report implemented

--- database/biomarket_10.12.2018_version13.sql
+++ database/biomarket_10.12.2018_version13.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 09, 2018 at 11:33 AM
+-- Generation Time: Dec 10, 2018 at 02:16 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `cart_tmp` (
   `prod_id` int(11) NOT NULL,
   `ord_qty` int(8) NOT NULL,
   PRIMARY KEY (`crt_ln`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `cart_tmp`
@@ -96,12 +96,7 @@ CREATE TABLE IF NOT EXISTS `customer_order` (
 --
 
 INSERT INTO `customer_order` (`ord_num`, `usr_id`, `ord_dte`, `sts`, `price_sum`, `adr_ln_1`, `adr_ln_2`, `pstcod`) VALUES
-('2_20181208233810', 2, '2018-12-08', 'DELIVERED', '22.80', '56 Staines Road', 'London', 'TW14 9HP'),
-('2_20181208233524', 2, '2018-12-08', 'DELIVERED', '0.86', '56 Staines Road', 'London', 'TW14 9HP'),
-('7_20181208232724', 7, '2018-12-08', 'DELIVERED', '85.52', '123', 'Feltham', 'cnvc'),
-('2_20181208233352', 2, '2018-12-08', 'DELIVERED', '40.24', '56 Staines Road', 'London', 'TW14 9HP'),
-('2_20181208233232', 2, '2018-12-08', 'DELIVERED', '45.30', '56 Staines Road', 'London', 'TW14 9HP'),
-('7_20181208232700', 7, '2018-12-08', 'DELIVERED', '25.10', '123', 'Feltham', 'cnvc');
+('9_20181210021455', 9, '2018-12-10', 'ORDERED', '6.64', '100 Staines Road', 'London', 'W5 5RF');
 
 -- --------------------------------------------------------
 
@@ -126,18 +121,19 @@ CREATE TABLE IF NOT EXISTS `customer_order_line` (
 --
 
 INSERT INTO `customer_order_line` (`ord_ln`, `ord_num`, `prod_id`, `ord_qty`, `sub_total`) VALUES
-(12, '2_20181208233810', 12, 4, '22.80'),
-(11, '2_20181208233524', 3, 1, '0.86'),
-(9, '2_20181208233352', 3, 8, '6.88'),
-(10, '2_20181208233352', 11, 12, '33.36'),
-(6, '2_20181208233232', 7, 10, '21.40'),
-(7, '2_20181208233232', 10, 4, '13.80'),
-(8, '2_20181208233232', 16, 1, '10.10'),
-(4, '7_20181208232724', 12, 12, '68.40'),
-(5, '7_20181208232724', 7, 8, '17.12'),
-(3, '7_20181208232700', 5, 1, '2.62'),
-(2, '7_20181208232700', 14, 8, '12.16'),
-(1, '7_20181208232700', 3, 12, '10.32');
+(50, '9_20181210021455', 5, 1, '2.62'),
+(51, '9_20181210021455', 13, 1, '4.02'),
+(35, '9_20181210011405', 1, 1, '1.25'),
+(36, '9_20181210011448', 15, 12, '42.72'),
+(38, '9_20181210012640', 5, 1, '2.62'),
+(37, '9_20181210012640', 7, 1, '2.14'),
+(41, '9_20181210013145', 4, 1, '1.25'),
+(40, '9_20181210013145', 5, 1, '2.62'),
+(42, '9_20181210013215', 4, 1, '1.25'),
+(45, '9_20181210014146', 2, 1, '2.03'),
+(44, '9_20181210014146', 4, 1, '1.25'),
+(48, '9_20181210014406', 13, 1, '4.02'),
+(49, '9_20181210014423', 5, 1, '2.62');
 
 -- --------------------------------------------------------
 
@@ -165,21 +161,21 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`prod_id`, `prod_name`, `price`, `sup_id`, `prod_dsc`, `prod_img`, `cat_id`, `sto_qty`) VALUES
-(1, 'Pomegrade Juice', '1.25', 2, 'Really nice product description for Pomegrade Juice', 'img/uploads/5bfc8efd684495.54781090.jpg', 2, 0),
-(2, 'Kale Juice', '2.03', 2, 'Really nice product description for Kale Juice', 'img/uploads/5bfc8ee02d5a58.55705998.jpg', 2, 0),
-(3, 'Carrot Juice', '0.86', 2, 'Really nice product description for Carrot Juice', 'img/uploads/5bfc8ebcb60fa4.65547830.jpg', 2, 114),
-(4, 'Broccoli and Ricotta Pie', '1.25', 1, 'Really nice product description for Broccoli and Ricotta Pie', 'img/uploads/5bfc8dc80e44b7.38413046.jpg', 1, 3),
-(5, 'Bread with Raisins', '2.62', 1, 'Really nice product description for Bread with Raisins', 'img/uploads/5bfc8b69202624.26787220.jpg', 1, 8),
-(6, 'Coconut Bars', '3.58', 1, 'Really nice product description for Coconut Bars', 'img/uploads/5bfc8dea460e81.62966147.jpg', 1, 11),
+(1, 'Pomegrade Juice', '1.25', 2, 'Really nice product description for Pomegrade Juice', 'img/uploads/5bfc8efd684495.54781090.jpg', 2, 11),
+(2, 'Kale Juice', '2.03', 2, 'Really nice product description for Kale Juice', 'img/uploads/5bfc8ee02d5a58.55705998.jpg', 2, 13),
+(3, 'Carrot Juice', '0.86', 2, 'Really nice product description for Carrot Juice', 'img/uploads/5bfc8ebcb60fa4.65547830.jpg', 2, 109),
+(4, 'Broccoli and Ricotta Pie', '1.25', 1, 'Really nice product description for Broccoli and Ricotta Pie', 'img/uploads/5bfc8dc80e44b7.38413046.jpg', 1, 119),
+(5, 'Bread with Raisins', '2.62', 1, 'Really nice product description for Bread with Raisins', 'img/uploads/5bfc8b69202624.26787220.jpg', 1, 121),
+(6, 'Coconut Bars', '3.58', 1, 'Really nice product description for Coconut Bars', 'img/uploads/5bfc8dea460e81.62966147.jpg', 1, 18),
 (7, 'French Baguette', '2.14', 1, 'Really nice product description for French Baguette', 'img/uploads/5bfc8e819496a5.59336623.jpg', 1, 181),
 (8, 'Yogurt', '0.50', 4, 'Really nice product description for Yogurt', 'img/uploads/5bfc93e69e7ae7.06241521.jpg', 4, 16),
-(9, 'Cottage Cheese', '4.15', 4, 'Really nice product description for Cottage Cheese', 'img/uploads/5bfc944b347bb6.96054639.jpg', 4, 7),
-(10, 'Goat Cheese', '3.45', 4, 'Really nice product description for Goat Cheese', 'img/uploads/5c0cf86ca07326.27274002.jpg', 4, 37),
-(11, 'Avocado', '2.78', 3, 'Really nice product description for Avocado', 'img/uploads/5bfc9547e90978.70031156.jpg', 3, 7),
-(12, 'Peas', '5.70', 3, 'Really nice product description for Peas', 'img/uploads/5bfc95aa96d079.50991718.jpg', 3, 0),
-(13, 'Beetroots', '4.02', 3, 'Really nice product description for Beetroots', 'img/uploads/5bfc961f9de346.08918563.jpg', 3, 11),
+(9, 'Cottage Cheese', '4.15', 4, 'Really nice product description for Cottage Cheese', 'img/uploads/5bfc944b347bb6.96054639.jpg', 4, 120),
+(10, 'Goat Cheese', '3.45', 4, 'Really nice product description for Goat Cheese', 'img/uploads/5c0cf86ca07326.27274002.jpg', 4, 42),
+(11, 'Avocado', '2.78', 3, 'Really nice product description for Avocado', 'img/uploads/5bfc9547e90978.70031156.jpg', 3, 9),
+(12, 'Peas', '5.70', 3, 'Really nice product description for Peas', 'img/uploads/5bfc95aa96d079.50991718.jpg', 3, 98),
+(13, 'Beetroots', '4.02', 3, 'Really nice product description for Beetroots', 'img/uploads/5bfc961f9de346.08918563.jpg', 3, 14),
 (14, 'Tomatoes', '1.52', 3, 'Really nice product description for Tomatoes', 'img/uploads/5bfc96d55d6458.60612011.jpg', 3, 2),
-(15, 'Lime and Apple Juice', '3.56', 2, 'Really nice product description for Lime and Apple Juice', 'img/uploads/5bfca66b203680.63516952.jpg', 2, 129),
+(15, 'Lime and Apple Juice', '3.56', 2, 'Really nice product description for Lime and Apple Juice', 'img/uploads/5bfca66b203680.63516952.jpg', 2, 119),
 (16, 'Quark Breakfast', '10.10', 4, 'Really nice product description for Quark Breakfast Selection', 'img/uploads/5bfca7094f5864.47772269.jpg', 4, 15),
 (17, 'Onions', '1.25', 3, 'Really nice product description for Onions', 'img/uploads/5bfca533eb0f58.64750428.jpg', 3, 79);
 
@@ -227,15 +223,13 @@ CREATE TABLE IF NOT EXISTS `supplier_ord` (
 --
 
 INSERT INTO `supplier_ord` (`sup_ord_id`, `sts`, `ord_dte`, `price_sum`) VALUES
-('5_20181207205734', 'CLOSED', '2018-12-07', '8.41'),
-('5_20181207205918', 'SUBMITED', '2018-12-07', '71.25'),
-('5_20181207210039', 'CLOSED', '2018-12-07', '88.84'),
-('5_20181207210614', 'SUBMITED', '2018-12-07', '27.33'),
-('5_20181207210659', 'CLOSED', '2018-12-07', '96.80'),
-('5_20181207213447', 'CLOSED', '2018-12-07', '1.25'),
-('5_20181207213914', 'SUBMITED', '2018-12-07', '29.40'),
-('5_20181208112709', 'CLOSED', '2018-12-08', '11.37'),
-('5_20181208113018', 'CLOSED', '2018-12-08', '5.56');
+('8_20181210000334', 'CLOSED', '2018-12-10', '5.53'),
+('5_20181209235615', 'CLOSED', '2018-12-09', '570.00'),
+('5_20181209235448', 'CLOSED', '2018-12-09', '38.70'),
+('5_20181209235410', 'CLOSED', '2018-12-09', '123.15'),
+('5_20181209235340', 'CLOSED', '2018-12-09', '65.91'),
+('5_20181209235321', 'CLOSED', '2018-12-09', '86.70'),
+('5_20181209235233', 'CLOSED', '2018-12-09', '6.81');
 
 -- --------------------------------------------------------
 
@@ -260,32 +254,33 @@ CREATE TABLE IF NOT EXISTS `supplier_ord_ln` (
 --
 
 INSERT INTO `supplier_ord_ln` (`sup_ord_id`, `sup_ord_ln`, `ord_qty`, `prod_id`, `price`) VALUES
-('5_20181207205734', 21, 2, 6, '7.16'),
-('5_20181207205734', 20, 1, 4, '1.25'),
-('5_20181207205918', 23, 5, 16, '50.50'),
-('5_20181207205918', 22, 5, 9, '20.75'),
-('5_20181207210039', 27, 6, 14, '9.12'),
-('5_20181207210039', 26, 6, 13, '24.12'),
-('5_20181207210039', 25, 6, 12, '34.20'),
-('5_20181207210039', 24, 5, 11, '13.90'),
-('5_20181207210039', 28, 6, 17, '7.50'),
-('5_20181207210614', 33, 3, 1, '3.75'),
-('5_20181207210614', 32, 3, 21, '6.00'),
-('5_20181207210614', 31, 3, 2, '6.09'),
-('5_20181207210614', 30, 9, 3, '7.74'),
-('5_20181207210614', 29, 3, 4, '3.75'),
-('5_20181207210659', 36, 5, 16, '50.50'),
-('5_20181207210659', 35, 5, 10, '17.25'),
-('5_20181207210659', 34, 7, 9, '29.05'),
-('5_20181207213447', 37, 1, 4, '1.25'),
-('5_20181207213914', 41, 3, 14, '4.56'),
-('5_20181207213914', 40, 3, 12, '17.10'),
+('8_20181210000334', 73, 1, 4, '1.25'),
+('8_20181210000334', 74, 2, 7, '4.28'),
+('5_20181209235615', 72, 100, 12, '570.00'),
+('5_20181209235448', 70, 10, 4, '12.50'),
+('5_20181209235448', 71, 10, 5, '26.20'),
+('5_20181209235410', 65, 12, 4, '15.00'),
+('5_20181209235410', 66, 10, 5, '26.20'),
+('5_20181209235410', 67, 15, 2, '30.45'),
+('5_20181209235410', 68, 8, 1, '10.00'),
+('5_20181209235410', 69, 10, 9, '41.50'),
+('5_20181209235340', 63, 15, 4, '18.75'),
+('5_20181209235340', 64, 18, 5, '47.16'),
+('5_20181209235321', 62, 5, 9, '20.75'),
+('5_20181209235321', 61, 5, 10, '17.25'),
+('5_20181209235321', 60, 5, 13, '20.10'),
+('5_20181209235321', 58, 6, 6, '21.48'),
+('5_20181209235321', 59, 2, 15, '7.12'),
+('5_20181209235233', 57, 2, 11, '5.56'),
+('5_20181209235233', 56, 1, 1, '1.25'),
 ('5_20181207213914', 39, 2, 5, '5.24'),
 ('5_20181207213914', 38, 2, 4, '2.50'),
 ('5_20181208112709', 44, 3, 14, '4.56'),
 ('5_20181208112709', 43, 2, 11, '5.56'),
 ('5_20181208112709', 42, 1, 4, '1.25'),
-('5_20181208113018', 45, 2, 11, '5.56');
+('5_20181208113018', 45, 2, 11, '5.56'),
+('5_20181209231438', 54, 1, 6, '3.58'),
+('5_20181209233922', 55, 1, 4, '1.25');
 
 -- --------------------------------------------------------
 
@@ -300,7 +295,19 @@ CREATE TABLE IF NOT EXISTS `sup_ord_tmp` (
   `ord_qty` int(8) NOT NULL,
   `prod_id` int(11) NOT NULL,
   PRIMARY KEY (`ord_ln`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmp_dob`
+--
+
+DROP TABLE IF EXISTS `tmp_dob`;
+CREATE TABLE IF NOT EXISTS `tmp_dob` (
+  `usr_id` int(11) NOT NULL,
+  `days` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -337,7 +344,7 @@ INSERT INTO `users` (`usr_id`, `f_name`, `l_name`, `e_mail`, `phn_num`, `dob`, `
 (4, 'Dominykas', 'Genys', 'dominykasgenysmail@gmail.com', '07401528222', '2018-11-06', '777', 'london', 'tw3 3ne', 'MEMBER', '$2y$10$lXc.2Ep53EIRVOvwc4YKp.TE8039.UE8iGYgF/3uFB9ph.3TIBzSS', '2018-12-01 16:01:44'),
 (5, 'AdminTester', 'Tester', '21353578@student.uwl.ac.uk', '07401528456', '2018-09-03', '12 kalo st.', 'london', 'W5 5RF', 'ADMIN', '$2y$10$LsFHtGnFwz8WnYfvkI8TTe0T7Bq99bt7MpadXcXU1H3aOhXU1qh3W', '2018-12-01 16:15:24'),
 (6, 'James', 'Evans', 'egri.editerika@sm.com', '242', '2018-12-04', 'cvnc', 'cvnc', 'cnvc', 'MEMBER', '$2y$10$bWP9ae.ZgmWtWKXtkyhJuuzXHytEENL8we8WHOx9UccRtoOUiTtpy', '2018-12-01 15:44:20'),
-(7, 'E', 'Lamm', 'lammmeer@gmail.com', '242', '2018-12-04', '123', 'Feltham', 'cnvc', 'MEMBER', '$2y$10$O2TAs54vH8qmlsw1axMcteoiF8.YWxA51j7FbmuwRI2fVkqv4CpvG', '2018-12-01 15:44:20'),
+(7, 'Era', 'Lamm', 'lammmeer@gmail.com', '242', '2018-12-04', '123', 'Feltham', 'cnvc', 'MEMBER', '$2y$10$O2TAs54vH8qmlsw1axMcteoiF8.YWxA51j7FbmuwRI2fVkqv4CpvG', '2018-12-01 15:44:20'),
 (8, 'Admin', 'Admin', 'admin', '1234', '2018-12-02', '123 road', 'London', 'W5 5RF', 'ADMIN', '$2y$10$wplUFF.dJLMHSmgFk74vEusuSNOQCJOKZ/oUxUd3rY120I4OAt3Wi', '2018-12-09 11:31:21'),
 (9, 'member', 'member', 'member', '123', '2018-12-02', '100 Staines Road', 'London', 'W5 5RF', 'MEMBER', '$2y$10$Kuif4Jbg4PM5wFmWRyTQneTjVpsuNiEh4RB0M2TwxLbJ2h/k57VTK', '2018-12-09 11:32:53');
 
--- php/acp_customers.php
+++ php/acp_customers.php
@@ -16,57 +16,125 @@
     
 ?>    
  <div class="orders_container">
-      <h2>Customers Report:</h2>  
+      <h2>Customer Report:</h2>
+<div class="row-stock customer-column2">
+  
+  
+
+  <div class="column two-stock2" >
+    <p>E-mail</p>
+  </div>
+  <div class="column unit2">
+    <p>Member since</p>
+  </div>
+    <div class="column unit2">
+    <p>Post Code</p>
+  </div>
+    <div class="column unit2">
+    <p>Orders Value</p>
+  </div>
+    <div class="column unit2">
+    <p>Date Of Birth</p>
+  </div>
+  <div class="column unit3">
+    <p>Days Until Birthday</p>
+  </div>
+
+
+
+
+</div>
+<?php	
+	$search0 = "SELECT usr_id, dob FROM users  WHERE lvl = 'MEMBER'";
+	$result0 = mysqli_query($mysqli, $search0);
+while ($row0 = mysqli_fetch_assoc($result0))
+		{
+				$cur_usr_id= $row0['usr_id'];
+				$cur_dob = $row0['dob'];
+				$cur_year = date("Y");
+				$birth_month = date("m",strtotime($cur_dob));
+				$birth_day = date("d",strtotime($cur_dob));
+				$leapyear = checkdate($birth_month ,$birth_day,$cur_year);
+				if($leapyear == True)
+				{
+				$cur_birthday = $cur_year . "-" . $birth_month . "-" . $birth_day;	
+				}
+				else
+				{
+				$birth_day =$birth_day - 1;	
+				$cur_birthday = $cur_year . "-" . $birth_month . "-" . $birth_day;
+				}
+				$days = strtotime($cur_birthday) - strtotime(date("Y-m-d"));
+				$days_to_bd = round($days / (60 * 60 * 24));
+				if ($days_to_bd >= 0)
+				{
+				$days_to_bd = $days_to_bd;	
+				}
+				else
+				{
+				$days_to_bd = $days_to_bd + 365;	
+				};
+				$search3 ="INSERT INTO tmp_dob (usr_id, days) VALUES('". $cur_usr_id ."','" . $days_to_bd . "')";
+				mysqli_query($mysqli, $search3);
+		}	
+
+
+ ?>
+	  
 <?php      
-$search1 = "SELECT u.f_name AS f_name, u.l_name AS l_name, u.acc_crt_dte AS acc_crt_dte, u.usr_id AS usr_id, CASE WHEN SUM(col.sub_total) > 0 THEN SUM(col.sub_total) ELSE 0 END as sub_total FROM users u  LEFT JOIN customer_order co ON u.usr_id = co.usr_id LEFT JOIN customer_order_line col ON co.ord_num = col.ord_num  WHERE u.lvl = 'MEMBER' GROUP BY u.usr_id ORDER BY sub_total DESC";
+$search1 = "SELECT  u.f_name AS f_name, u.e_mail as e_mail, u.l_name AS l_name, u.acc_crt_dte AS acc_crt_dte, u.pstcod AS pstcod, u.usr_id AS usr_id, CASE WHEN SUM(col.sub_total) > 0 THEN SUM(col.sub_total) ELSE 0 END as sub_total, u.dob AS dob , td.days as days FROM users u  LEFT JOIN tmp_dob td ON u.usr_id = td.usr_id LEFT JOIN customer_order co ON u.usr_id = co.usr_id LEFT JOIN customer_order_line col ON co.ord_num = col.ord_num  WHERE u.lvl = 'MEMBER' GROUP BY u.usr_id ORDER BY td.days ASC, sub_total DESC";
 $result1 = mysqli_query($mysqli, $search1);
 while ($row1 = mysqli_fetch_assoc($result1)) 
 {
 $cur_user = $row1['f_name']. " " . $row1['l_name'];
-$cur_acc_crt_dte = $row1['acc_crt_dte'] ;
+$cur_acc_crt_dte = $row1['acc_crt_dte'];
 $cur_sub_total = $row1['sub_total'];    
-$cur_usr_id = $row1['usr_id'];     
+$cur_pstcod = $row1['pstcod'];
+$cur_dob = $row1['dob'];
+$cur_days =  $row1['days'];
+$cur_emial = $row1['e_mail'];  
 ?>  
 <div class="row-stock customer-column">
 
-  <div class="column two-stock">
-    <p><?php echo $cur_user ?></p>
+  
+   <div class="column two-stock2">
+    <p><?php echo $cur_emial ?></p>
    
   </div>
 
   <div class="column unit2">
    
-    <p>Member since: </p>
-  </div>
-  <div class="column unit2">
-    
     <p><?php $joindate = strtotime($cur_acc_crt_dte);
                       echo date("d/m/Y", $joindate) ;?></p>
   </div>
   <div class="column unit2">
+    
+    <p><?php echo $cur_pstcod; ?></p>
+	
+  </div>
+  <div class="column unit2">
    
-    <p>Orders Value: </p>
+    <p>&pound <?php echo $cur_sub_total; ?></p>
   </div>
 
   <div class="column unit2">
      
-    <p>&pound <?php echo $cur_sub_total; ?></p>
-  </div>  
-
-  <div class="column unit2">
-<?php   
-   print '<div class="btn-details">
-         
-   <a href="?page=acp_customers&usr_id=' . $cur_usr_id . '&action=see_details">Details</a></div> ';  
-   
-?>      
+    <p><?php echo $cur_dob; ?></p>
+  </div>
+  <div class="column unit3">
      
-  </div>  
+    <p><?php echo $cur_days;	?> days</p>
+  </div>    
+
+ 
 
 </div>
 <?php
  
-  } 
+  }
+
+	$sql1 = "DELETE FROM tmp_dob";
+	mysqli_query($mysqli, $sql1);
   
 ?>
 </div>      
